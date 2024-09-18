6c16ff8a231a0f5811364e3db4d4dfe6d70590c6
Emilio Gil del Rio <emilio.gildelrio@opendeusto.es>
2024-09-18 11:38:07 +0200

    se termina

--- PruebaGit/src/Principal/HolaMundo.java
+++ PruebaGit/src/Principal/HolaMundo.java
@@ -2,6 +2,8 @@ package Principal;
 
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Adios mundo");
+		System.out.println("Hola andertxo");
+		System.out.println("Se termina la clase");
 	}
 }
