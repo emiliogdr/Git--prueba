9ed396697518fcd4d7506989b7e241cddf58e285
emilio <emilio.gildelrio@opendeusto.es>
2024-09-23 08:23:06 +0200

    Update HolaMundo.java

--- PruebaGit/src/Principal/HolaMundo.java
+++ PruebaGit/src/Principal/HolaMundo.java
@@ -4,7 +4,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Adios mundo");
 		System.out.println("Hola andertxo");
-		System.out.println("empieza la clase");
 	
 	}
 }
